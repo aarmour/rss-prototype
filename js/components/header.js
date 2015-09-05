b069704cdab44cdd6a06e461a012794c1eb9f4d1
Adam Armour <adam.armour@gmail.com>
2015-09-04 22:55:12 -0600

    feat: basic header

--- js/components/header.js
+++ js/components/header.js
@@ -1,3 +1,28 @@
-export default function Header() {
+import componentHandler from '../componentHandler';
 
+const HEADER_HEIGHT = 50;
+
+// CSS classes
+const FADE = 'rss-header--fade';
+
+export default function Header(element) {
+  this.element = element;
+  this.init();
 }
+
+Header.prototype.init = function () {
+  if (!this.element || !this.element.classList.contains(FADE)) return;
+  let navEl = this.element.querySelector('nav');
+  if (!navEl) return;
+
+  window.addEventListener('scroll', () => {
+    let alpha = window.scrollY > HEADER_HEIGHT ? '1' : '0';
+    navEl.style['background-color'] = `rgba(240, 240, 240, ${alpha})`;
+  });
+};
+
+componentHandler.register({
+  constructor: Header,
+  classAsString: 'Header',
+  cssClass: 'rss-header'
+});
